267fbb674b8a189c09605dd0ff71f6ca087d7ccb
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2022-10-24 21:40:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let showTime = () => {
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    let Clock = document.getElementById('timer');
+
+    Clock.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(showTime, 1000);
